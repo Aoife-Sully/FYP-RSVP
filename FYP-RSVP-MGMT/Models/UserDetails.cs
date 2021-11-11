1f6e1ec5603414de730210c2056f73ed800d66f7
aoifesully999@gmail.com <118308233@umail.ucc.ie>
2021-11-11 18:02:13 +0000

    1.5.1  Made changes to bootstrap Add new user table - for login Edited pages Still cannot add to db from view

---
+++ FYP-RSVP-MGMT/Models/UserDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FYP_RSVP_MGMT.Models
+{
+    public class UserDetails
+    {
+        public int UserID { get; set; }
+
+        public int GuestID { get; set; }
+
+        [Required]
+        public string GuestName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
